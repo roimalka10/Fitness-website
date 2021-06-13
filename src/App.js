c1ff0edce42b89237277ac07270d8eb680cb4ab1
Roi malka <roimalka10@gmail.com>
2021-06-13 03:20:59 +0300

    added first home page section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import "./App.css";
+import React from "react";
+import "./App.scss";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Home />
+      </Router>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,68 @@
+import React from "react";
+import "./Home.scss";
+import { motion } from "framer-motion";
+
+const Home = () => {
+  return (
+    <>
+      <section className="homepage" name="home-id">
+        <div className="black"></div>
+        <div className="home-content">
+          <div className="home-content-main">
+            <motion.div
+              className="home-title"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{
+                duration: 0.5,
+                delay: 1.2,
+              }}
+            >
+              <h1 className="h1">PERSONAL TRAINING</h1>
+            </motion.div>
+            <motion.div
+              className="home-subtitle"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{
+                duration: 0.5,
+                delay: 1.4,
+              }}
+            >
+              <h3 className="h3">
+                Premium weight loss and lifestyle transformations
+              </h3>
+            </motion.div>
+            <div className="btn-group">
+              <motion.a
+                className="btn left"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{
+                  duration: 0.5,
+                  delay: 1.6,
+                }}
+              >
+                Start here
+              </motion.a>
+              <motion.a
+                className="btn"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{
+                  duration: 0.5,
+                  delay: 1.6,
+                }}
+              >
+                Get in touch
+              </motion.a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className="blue-line"></div>
+    </>
+  );
+};
+
+export default Home;
