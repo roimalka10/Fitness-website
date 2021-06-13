9606834203684bac8df3ee96f584c2e376aaa7be
Roi malka <roimalka10@gmail.com>
2021-06-14 01:58:11 +0300

    added 2nd section to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -65,6 +65,38 @@ const Home = () => {
         </div>
       </section>
       <div className="blue-line"></div>
+      <section className="believe">
+        <div>
+          <h2 className="h2">What i believe</h2>
+        </div>
+        <div>
+          <p>
+            At Strength Coach Glasgow, we believe that to be successful in
+            achieving your goals, you have to cut through all the fads and go
+            straight to the proven, core principles of fitness and health. We
+            know that the way to get results is to focus on strategies that
+            yield a high return and cut out the nonsense.
+            <br />
+            <br /> Our philosophy is to keep it as simple as possible, making it
+            easier for you to learn the ropes and get sustainable results. We
+            give you the tools you just need to take steps towards your goals
+            through consistent hard work and dedication.
+            <br />
+            <br /> Here at Strength Coach Glasgow, we’re all about helping
+            people like you gain confidence, appreciate yourself for what you
+            can do and rediscover your youth. Thanks to our range of weekly
+            programs, you’ll learn how to do exercises properly for maximum
+            benefit to your physique, as well as get smartphone app access to
+            tailored training regimes to suit your fitness level and desired
+            pace.
+          </p>
+        </div>
+        <a className="btn">
+          Contact
+          <span className="border-top-bottom"></span>
+          <span className="border-left-right"></span>
+        </a>
+      </section>
     </>
   );
 };
